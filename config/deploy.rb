b0e0983aeedb0fea8a00096986198587d5136c34
Shane O'Grady <shane.ogrady@gmail.com>
2013-11-25 21:33:09 -0200

    Configure Capistrano
    You must replace `deploy@example.com` with your own server address

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'c3-puma-test'
+set :repo_url, 'https://github.com/shaneog/c3-puma-test.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, "/webapps/#{fetch(:application)}"
 # set :scm, :git
 
 # set :format, :pretty
