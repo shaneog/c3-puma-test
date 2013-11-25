9f6d723af019f808c7f1a36c5c9434918ba3fdef
Shane O'Grady <shane.ogrady@gmail.com>
2013-11-25 20:54:16 -0200

    Initial Commit
    App created with
    ```rails new c3-puma-test --skip-active-record --skip-test-unit```

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+C3PumaTest::Application.config.secret_key_base = '22f8edaa51b5a11e74354e9d1e026f34364f0b0a680b6948e6d88309c784adf76a720cf626e78af29d35be767b5576c42a9af565d22ba1817cc1cb5e78eb9dc9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+C3PumaTest::Application.config.session_store :cookie_store, key: '_c3-puma-test_session'
